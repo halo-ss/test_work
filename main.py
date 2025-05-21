ffd68ebc821767580a327167f2f8e14b67d82df3
halo-ss <57302991+halo-ss@users.noreply.github.com>
2025-05-21 12:25:40 +0300

    add comment

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def check_table_bot(user_question):
     code_block = result_str.split("</think>")[1]
     
     return code_block
+# вторая ступень обработки вопроса вывод результата на экран
 def result_bot(user_question):
     model = lms.llm("qwen3-14b")
     
@@ -43,14 +44,24 @@ def result_bot(user_question):
     code_block = result_str.split("</think>")[1]
     
     return print(code_block)
+
+#чтение базы
 df = pd.read_csv("freelancer_earnings_bd.csv")
+
+# получение текста
 input_text = input("Введите запрос:")
+
+# получение таблицы 
 code_block = check_table_bot(input_text)
 # print(code_block)
-exec(code_block)  
+
+# Компиляция 
+exec(code_block)
+# Объединение таблицы и запроса   
 user_question = f"""
 таблица: {df_result.to_string(index=False)}
 запрос: {input_text}
 """
+#вывод результатов
 result_bot(user_question)
 print(df_result)
